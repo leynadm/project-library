8d51d2bd0ffedfb8e584b6dedae8c1fa2277692e
Leynad <matei_daniel@outlook.com>
2022-09-19 21:02:16 +0200

    Adjust layout structure, build initial function logic

--- javascript.js
+++ javascript.js
@@ -1,3 +1,27 @@
+// Page elements
+const btn_add_book = document.getElementById('add-to-library');
+const books_container = document.querySelector('.books-container');
+const books_tile = document.querySelector('.book-tile');
+
+let myLibrary=[];
+
+btn_add_book.addEventListener('click',function(){
+
+    let book_name_input2 = document.querySelector('#book-name').value;
+    let book_author_input = document.querySelector('#author-name').value;
+    let book_pages_input = document.querySelector('#no-pages').value;
+    let book_read_status = document.querySelector('#read-status');   
+
+    let newBook = new Book(book_name_input2,"me","232",false);
+
+    myLibrary.push(newBook);
+    
+    console.log(myLibrary);
+})
+
+const book_tile_div = document.createElement('div');
+book_tile_div.classList.add('book-tile');
+
 function Book(title, author, pages, readStatus, info){
     this.title= title
     this.author= author
@@ -9,8 +33,21 @@ function Book(title, author, pages, readStatus, info){
     }
 }
 
-let myLibrary=[];
 
 function addBookToLibrary(){
 
+    const book_tile_div = document.createElement('div');
+    book_tile_div.classList.add('book-tile');
+
+    const book_title_div = document.createElement('div');
+    book_title_div.classList.add('tile-title');
+    book_title_div.textContent = "Book.title";
+    
+    book_tile_div.appendChild(book_title_div);
+
+
+    books_container.appendChild(book_tile_div);
+    
 }
+
+let Book1 = new Book("Me", "You","232",true)
